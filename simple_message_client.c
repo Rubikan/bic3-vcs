cdf370998d1b21b48ba955ee5c1cc6c27cbf7760
Rubikan <rubikandreas@gmail.com>
2015-11-18 18:35:16 +0100

    added doxygen comments

--- simple_message_client.c
+++ simple_message_client.c
@@ -1,4 +1,13 @@
-//
-// Created by Andreas Rubik on 17.11.15.
-//
+/**
+ *
+ * @file simple_message_client.c
+ * Simple Message Client
+ *
+ * @author Andreas Rubik <andreas.rubik@technikum-wien.at>
+ * @author Anna Glock <ic14b070@technikum-wien.at>
+ *
+ * @version 1.0
+ * @date 2015/11/18
+ *
+ */
 
